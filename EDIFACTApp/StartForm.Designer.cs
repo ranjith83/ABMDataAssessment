9a7ee4aa2a539380363b29650b7c18a9a90273dc
Ranjith <ranji_b4u@yahoo.co.in>
2019-04-26 01:09:17 +0200

    rename the files

--- EDIFACTApp/StartForm.Designer.cs
+++ EDIFACTApp/StartForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EDIFACTApp
 {
-    partial class Form1
+    partial class StartForm
     {
         /// <summary>
         /// Required designer variable.
--- EDIFACTApp/StartForm.cs
+++ EDIFACTApp/StartForm.cs
@@ -16,9 +16,9 @@ using System.Xml.Linq;
 
 namespace EDIFACTApp
 {
-    public partial class Form1 : Form
+    public partial class StartForm : Form
     {
-        public Form1()
+        public StartForm()
         {
             InitializeComponent();
         }
